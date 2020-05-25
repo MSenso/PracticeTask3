74008ce523a7e38d6a947309f70166dcb418a0e0
skjfdnv <skjfdnv@yandex.ru>
2020-05-25 23:24:33 +0500

    Upd

--- PracticeTask3/Form1.Designer.cs
+++ PracticeTask3/Form1.Designer.cs
@@ -28,13 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.XInput = new System.Windows.Forms.TextBox();
+            this.YInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.UOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(131, 513);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ввод x:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(130, 555);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ввод y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(205, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "График области D:";
+            // 
+            // XInput
+            // 
+            this.XInput.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.XInput.Location = new System.Drawing.Point(222, 511);
+            this.XInput.Name = "XInput";
+            this.XInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.XInput.Size = new System.Drawing.Size(281, 36);
+            this.XInput.TabIndex = 4;
+            this.XInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.XInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.XInput_KeyDown);
+            // 
+            // YInput
+            // 
+            this.YInput.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YInput.Location = new System.Drawing.Point(222, 553);
+            this.YInput.Name = "YInput";
+            this.YInput.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.YInput.Size = new System.Drawing.Size(281, 36);
+            this.YInput.TabIndex = 5;
+            this.YInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.YInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.YInput_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(120, 619);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "u равно:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::PracticeTask3.Properties.Resources.Plot;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 463);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UOutput
+            // 
+            this.UOutput.BackColor = System.Drawing.Color.White;
+            this.UOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UOutput.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.UOutput.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UOutput.Location = new System.Drawing.Point(222, 619);
+            this.UOutput.Name = "UOutput";
+            this.UOutput.Size = new System.Drawing.Size(281, 33);
+            this.UOutput.TabIndex = 8;
+            this.UOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::PracticeTask3.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(622, 683);
+            this.Controls.Add(this.UOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.YInput);
+            this.Controls.Add(this.XInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Вычисление u";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox XInput;
+        private System.Windows.Forms.TextBox YInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label UOutput;
     }
 }
 
--- PracticeTask3/Form1.cs
+++ PracticeTask3/Form1.cs
@@ -12,9 +12,89 @@ namespace PracticeTask3
 {
     public partial class Form1 : Form
     {
+        double x, y, u, e = 0.01;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                YInput.Text = YInput.Text.Replace('.', ',');
+                if (UOutput.Text == string.Empty)
+                {
+                    if (double.TryParse(YInput.Text, out y))
+                    {
+                        XInput.Text = XInput.Text.Replace('.', ',');
+                        if (double.TryParse(XInput.Text, out x))
+                        {
+                            UOutput.Focus();
+                            U_Calculation();
+                        }
+                        else XInput.Focus();
+                    }
+                    else MessageBox.Show("Введите вещественное число!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    UOutput.Text = string.Empty;
+                    XInput.Focus();
+                }
+            }
+        }
+
+        private void UOutput_Click(object sender, EventArgs e)
+        {
+        }
+
+        void U_Calculation()
+        {
+            double approximate_value = Math.Pow(x, 2);
+            if (Math.Abs(Math.Round(Math.Pow(x, 2), 2) - Math.Pow(x, 2)) <= e) approximate_value = Math.Round(Math.Pow(x, 2), 2);
+            bool first_limit = y > approximate_value;
+            approximate_value = Math.Pow(Math.E, x);
+            if (Math.Abs(Math.Round(Math.Pow(x, 2), 2) - Math.Pow(x, 2)) <= e) approximate_value = Math.Round(Math.Pow(Math.E, x), 2);
+            bool second_limit = y < approximate_value;
+            approximate_value = Math.Pow(Math.E, -x);
+            if (Math.Abs(Math.Round(Math.Pow(x, 2), 2) - Math.Pow(x, 2)) <= e) approximate_value = Math.Round(Math.Pow(Math.E, -x), 2);
+            bool third_limit = y < approximate_value;
+            if (first_limit && second_limit && third_limit)
+                u = x + y;
+            else u = x - y;
+            UOutput.Text = u.ToString();
+        }
+        private void XInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (UOutput.Text == string.Empty)
+                {
+                    XInput.Text = XInput.Text.Replace('.', ',');
+                    if (double.TryParse(XInput.Text, out x))
+                    {
+                        YInput.Text = YInput.Text.Replace('.', ',');
+                        if (double.TryParse(YInput.Text, out y))
+                        {
+                            UOutput.Focus();
+                            U_Calculation();
+                        }
+                        else YInput.Focus();
+                    }
+                    else MessageBox.Show("Введите вещественное число!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    UOutput.Text = string.Empty;
+                    YInput.Focus();
+                }
+            }
         }
     }
 }
